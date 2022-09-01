513ccf5b1e25ee8142a08ed494767e37392b47e0
SeiyaIwabuchi <0123ook@gmail.com>
2022-09-01 08:29:33 +0000

    release

--- src/config_loader.py
+++ src/config_loader.py
@@ -1,16 +1,22 @@
 import os
 
-from src.config import Config
-from src.param_source import ParamSource
+from config import Config
+from param_source import ParamSource
 
+
+def get_os_environ(name: str):
+    try:
+        return os.environ[name]
+    except KeyError:
+        return None
 
 class ConfigLoader:
     config:Config = None
 
     @staticmethod
     def load_param_source():
-        param_source = os.environ["param_source"]
-        if os.environ["param_source"] is not None:
+        param_source = get_os_environ("param_source")
+        if get_os_environ("param_source") is not None:
             if param_source in [ParamSource.config_file, ParamSource.environment]:
                 Config.param_source = param_source
             else:
@@ -25,8 +31,10 @@ class ConfigLoader:
         elif Config.param_source == ParamSource(ParamSource.environment):
             ConfigLoader.config = Config
             for filed_name in Config.__dict__.keys():
-                ConfigLoader.config.__setattr__(filed_name, os.environ[filed_name])
+                ConfigLoader.config.__setattr__(filed_name, get_os_environ(filed_name))
         else:
             raise Exception(f'Unknown param source. {Config.param_source.param_source}')
         
         ConfigLoader.config.rest_server_url.rstrip("/")
+
+        return ConfigLoader.config
--- src/main.py
+++ src/main.py
@@ -3,14 +3,15 @@ main.py
 """
 import json
 from typing import Dict
-from flask import Flask, render_template
+from flask import Flask, render_template, Markup
 import requests
+from markdown import markdown
 
 from config_loader import ConfigLoader
 
 app = Flask(__name__)
-CONFIG = ConfigLoader.config
-base_url = CONFIG.rest_server_url
+CONFIG = ConfigLoader.load()
+base_url = CONFIG.rest_server_url + CONFIG.rest_url_prefix
 
 def with_query_strings(url: str, query_strings: Dict[str, str]) -> str:
     string = ""
@@ -21,15 +22,23 @@ def with_query_strings(url: str, query_strings: Dict[str, str]) -> str:
 
 @app.route('/pages')
 def pages():
-    request_url = with_query_strings(f'{base_url}{CONFIG.endpoint_pages}', {'access_token':CONFIG.api_key})
-    raw_json = requests.get(request_url)
+    request_url = with_query_strings(f'{base_url}{CONFIG.endpoint_pages}', {'access_token':CONFIG.api_key, 'path':'/'})
+    raw_json = requests.get(request_url).text
     enc_json = json.loads(raw_json)
     return render_template('pages.html', pages=enc_json['pages'])
 
-app.run(host=CONFIG.host_name, port=CONFIG.port)
+@app.route('/v3/page/<page_id>')
+def page(page_id):
+    request_url = with_query_strings(f'{base_url}{CONFIG.endpoint_page}', {'access_token':CONFIG.api_key, 'pageId':page_id})
+    raw_json = requests.get(request_url).text
+    enc_json = json.loads(raw_json)
+    print(enc_json)
+    return render_template(
+        'page.html',
+        md_text=Markup(markdown(enc_json['page']['revision']['body'])),
+        rest_server_url=CONFIG.rest_server_url,
+        page_id=page_id
+        )
 
-# @app.route('/page/<page_id>')
-# def page(page_id):
-#     # 本文を入れたうえでリダイレクトする
-#     requests.get()
-#     pass
+if __name__ == '__main__':
+    app.run(host=CONFIG.host_name, port=CONFIG.port)
